279cf70c6002f69981afdbe4da04af8032711d1f
Sonu Singh <shadow.pentest1899@gmail.com>
2026-04-07 09:36:08 +0530

    this is how to impliment larraylist in collections

---
+++ List/arraylist1.java
@@ -0,0 +1,16 @@
+package List;
+
+import java.util.ArrayList;
+
+public class arraylist1 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        System.out.println(list);
+    }
+}
